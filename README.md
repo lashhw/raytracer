7d45b8666bfd9bc67bae4cc82bb842d4f7524ffb
Jacky Huang <lashhw81@gmail.com>
2022-06-17 20:21:34 +0800

    cornell box with balls

---
+++ README.md
@@ -0,0 +1,20 @@
+# raytracer
+This is my implementation of physically based path tracer.
+
+## Build
+```bash
+mkdir build
+cd build
+cmake ..
+make
+```
+
+## Usage
+```bash
+./raytracer
+```
+
+## Result
+| Cornell Box (1000 samples/pixel)     |
+|--------------------------------------|
+| ![](https://i.imgur.com/bBioiFp.png) |
--- main.cpp
+++ main.cpp
@@ -36,6 +36,7 @@ int main() {
     std::ofstream file("image.ppm");
 
     /*
+    // scene with lots of balls
     const double ASPECT_RATIO = 16.0 / 9.0;
     const int image_width = 500;
     const int image_height = image_width / ASPECT_RATIO;
@@ -66,8 +67,8 @@ int main() {
     double aspect_ratio = 1.0;
     int image_width = 600;
     int image_height = image_width / aspect_ratio;
-    int samples_per_pixel = 200;
-    int max_depth = 8;
+    int samples_per_pixel = 1000;
+    int max_depth = 16;
     vec3 background = vec3(0, 0, 0);
     vec3 lookfrom(278, 278, -800), lookat(278, 278, 0);
     double vfov = 40.0;
@@ -79,12 +80,13 @@ int main() {
     shared_ptr<material> light = make_shared<diffuse_light>(vec3(15, 15, 15));
 
     hittable_list world;
-    world.add(make_shared<xz_rect>(0, 555, 0, 555, 555, green));
-    world.add(make_shared<xz_rect>(0, 555, 0, 555, 0, red));
+    world.add(make_shared<xz_rect>(0, 555, 0, 555, 555, red));
+    world.add(make_shared<xz_rect>(0, 555, 0, 555, 0, green));
     world.add(make_shared<yz_rect>(213, 343, 227, 332, 1, light));
     world.add(make_shared<yz_rect>(0, 555, 0, 555, 0, white));
     world.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
     world.add(make_shared<yz_rect>(0, 555, 0, 555, 555, white));
+    /*
     shared_ptr<hittable> box1 = make_shared<box>(vec3(0, 0, 0), vec3(330, 165, 165), white);
     box1 = make_shared<rotate_x>(box1, 15);
     box1 = make_shared<translate>(box1, vec3(225, 265, 295));
@@ -93,6 +95,15 @@ int main() {
     box2 = make_shared<rotate_x>(box2, -18);
     box2 = make_shared<translate>(box2, vec3(390, 130, 65));
     world.add(box2);
+     */
+    shared_ptr<material> mirror_material = make_shared<metal>(vec3(0.8, 0.8, 0.9), 0.0);
+    shared_ptr<material> ball_material = make_shared<metal>(vec3(0.9, 0.73, 0.05), 1.0);
+    shared_ptr<material> glass_material = make_shared<dielectric>(1.5);
+    shared_ptr<material> earth_material = make_shared<lambertian>(make_shared<image_texture>("../earthmap.jpg"));
+    world.add(make_shared<sphere>(vec3(475, 175, 300), 80, mirror_material));
+    world.add(make_shared<sphere>(vec3(475, 380, 150), 80, ball_material));
+    world.add(make_shared<sphere>(vec3(515, 100, 80), 40, earth_material));
+    world.add(make_shared<sphere>(vec3(200, 350, 100), 80, glass_material));
 
     camera cam(
             lookfrom, lookat, vec3(-1, 0, 0),
